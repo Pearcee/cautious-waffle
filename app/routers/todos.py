4af93eb6be4012dbe6d3946957f1b49f983a635d
Steve <sjpearce@gmail.com>
2025-06-06 09:50:37 +0100

    3

--- app/routers/todos.py
+++ app/routers/todos.py
@@ -8,7 +8,7 @@ router = APIRouter(tags=["todos"],prefix="/todos")
 
 
 @router.post("/")
-async def add_user(request:TodoSchema, db: Session = Depends(get_db)):
+async def add_todo(request:TodoSchema, db: Session = Depends(get_db)):
     todo = Todo(title=request.title, completed=request.completed, createdat=request.createdat)
     db.add(todo)
     db.commit()
@@ -16,17 +16,17 @@ async def add_user(request:TodoSchema, db: Session = Depends(get_db)):
     return todo
 
 @router.get("/")
-async def get_users(db: Session = Depends(get_db)):
+async def get_todos(db: Session = Depends(get_db)):
     todos = db.query(Todo).all()
     return todos
 
 @router.get("/{name}")
-async def get_users(title,db: Session = Depends(get_db)):
+async def get_todos(title,db: Session = Depends(get_db)):
     todo = db.query(Todo).filter(Todo.title == title).first()
     return todo
 
 @router.delete("/{id}")
-async def get_users(id,db: Session = Depends(get_db)):
+async def get_todos(id,db: Session = Depends(get_db)):
     todo = db.query(Todo).filter(Todo.id== id).delete()
     if not todo:
         return {"message": "Todo not found"}
@@ -34,7 +34,7 @@ async def get_users(id,db: Session = Depends(get_db)):
     return {"message": "Todo deleted successfully"}
 
 @router.put("/{id}")
-async def update_user(id, request: TodoSchema, db: Session = Depends(get_db)):
+async def update_todo(id, request: TodoSchema, db: Session = Depends(get_db)):
     todo = db.query(Todo).filter(Todo.id == id).first()
     if not todo:
         return {"message": "Todo not found"}
